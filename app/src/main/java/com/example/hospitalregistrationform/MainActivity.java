ad8974250a2c82a364cc6ec36fedbe5a92584cb5
Sudeeppudasaini <heroclan82@gmail.com>
2025-07-12 21:58:07 +0545

    new

--- app/src/main/java/com/example/hospitalregistrationform/MainActivity.java
+++ app/src/main/java/com/example/hospitalregistrationform/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     EditText name, age, gender, address, phone;
 
     Button registerBtn;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
