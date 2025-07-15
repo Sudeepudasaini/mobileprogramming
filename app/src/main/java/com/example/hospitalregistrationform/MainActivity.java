017537113d6ec2d82c548ce219cf6261a328c779
Sudeeppudasaini <heroclan82@gmail.com>
2025-07-15 21:01:54 +0545

    add

--- app/src/main/java/com/example/hospitalregistrationform/MainActivity.java
+++ app/src/main/java/com/example/hospitalregistrationform/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText name, age, gender, address, phone;
 
-    Button registerBtn;
+    registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
