11dc13c6798e9adb91d459819b606e45cb6401c9
Sudeeppudasaini <heroclan82@gmail.com>
2025-07-30 21:26:43 +0545

    new

--- app/src/main/java/com/example/hospitalregistrationform/MainActivity.java
+++ app/src/main/java/com/example/hospitalregistrationform/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         name= findViewById(R.id.etName);
         age= findViewById(R.id.etAge);
-        gender = findViewById(R.id.etGender);
+         = findViewById(R.id.etGender);
         address= findViewById(R.id.etAddress);
         phone = findViewById(R.id.etPhone);
         registerBtn = findViewById(R.id.btnRegister);
